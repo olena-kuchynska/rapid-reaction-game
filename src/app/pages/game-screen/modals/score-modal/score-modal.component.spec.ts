c96a6cfc657953e15afb9d070063d1512c93ab9b
Olena Bulatova <bulatova853@gmail.com>
2024-06-10 02:48:22 +0300

    Add tests

--- src/app/pages/game-screen/modals/score-modal/score-modal.component.spec.ts
+++ src/app/pages/game-screen/modals/score-modal/score-modal.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { MockProvider } from 'ng-mocks';
+import { BsModalRef } from 'ngx-bootstrap/modal';
 
 import { ScoreModalComponent } from './score-modal.component';
 
@@ -10,6 +12,7 @@ describe('ScoreModalComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [ScoreModalComponent],
+      providers: [MockProvider(BsModalRef, { hide: () => {} })],
       schemas: [NO_ERRORS_SCHEMA]
     });
     fixture = TestBed.createComponent(ScoreModalComponent);
@@ -19,5 +22,13 @@ describe('ScoreModalComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should hide modal', () => {
+    const spy = spyOn(component.bsModalRef, 'hide');
+
+    component.hide();
+
+    expect(spy).toHaveBeenCalled();
   });
 });
